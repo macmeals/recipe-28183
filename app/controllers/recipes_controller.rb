654b21e7f0d0204f96a0a2543147fce5ecbdf234
macmeals <loseworld9731@gmail.com>
2020-12-13 22:04:37 +0900

    racipeのレコード再作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,11 +14,11 @@ class RecipesController < ApplicationController
 
   private
    def recipe_params
-    params.require(:recipe).permit(:name, :category_id, :explaination, :detail1, :detail2, :detail3, :detail4, :detail5).merge(user_id: current_user.id)
+    params.permit(:name, :category_id, :explaination, :detail1, :detail2, :detail3, :detail4, :detail5, :images[] ).merge(user_id: current_user.id)
    end
 
    def ingredient_params(recipe)
-    params.require(:ingredient).permit(:name1, :quantity1, :calorie1, :name2, :quantity2, :calorie2 ,:name3, :quantity3, :calorie3,:name4, :quantity4, :calorie4, :name5, :quantity5, :calorie5 ).merge(recipe_id: recipe.id)
+    params.permit(:name1, :quantity1, :calorie1, :name2, :quantity2, :calorie2 ,:name3, :quantity3, :calorie3,:name4, :quantity4, :calorie4, :name5, :quantity5, :calorie5 ).merge(recipe_id: recipe.id)
    end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+  has_many_attached :images
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
@@ -7,4 +9,5 @@ class Recipe < ApplicationRecord
   has_one :ingredient
 
   validates :category_id , numericality: { other_than: 1 } 
+
 end
